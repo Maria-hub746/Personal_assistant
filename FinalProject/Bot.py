c772c14bfcb99146585719f5c78e49cb84f66d8b
Maria-hub746 <113303447+Maria-hub746@users.noreply.github.com>
2022-11-27 21:01:38 +0300

    Python

---
+++ FinalProject/Bot.py
@@ -0,0 +1,114 @@
+from AddressBook import *
+from abc import ABC, abstractmethod
+
+
+class Handles(ABC):
+    def __init__(self):
+        self.book = AddressBook()
+
+    @abstractmethod
+    def func(self, action):
+        pass
+
+
+class Add(Handles):
+    def func(self, action):
+        name = Name(input("Name: ")).value.strip()
+        phones = Phone().value
+        birth = Birthday().value
+        email = Email().value.strip()
+        status = Status().value.strip()
+        note = Note(input("Note: ")).value
+        record = Record(name, phones, birth, email, status, note)
+        return self.book.add(record)
+
+
+class Search(Handles):
+    def func(self, action):
+        print("There are following categories: \nName \nPhones \nBirthday \nEmail \nStatus \nNote")
+        category = input('Search category: ')
+        pattern = input('Search pattern: ')
+        result = (self.book.search(pattern, category))
+        for account in result:
+            if account['birthday']:
+                birth = account['birthday'].strftime("%d/%m/%Y")
+                result = "_" * 50 + "\n" + f"Name: {account['name']} \nPhones: {', '.join(account['phones'])} \nBirthday: {birth} \nEmail: {account['email']} \nStatus: {account['status']} \nNote: {account['note']}\n" + "_" * 50
+                print(result)
+
+
+class Edit(Handles):
+    def func(self, action):
+        contact_name = input('Contact name: ')
+        parameter = input('Which parameter to edit(name, phones, birthday, status, email, note): ').strip()
+        new_value = input("New Value: ")
+        return self.book.edit(contact_name, parameter, new_value)
+
+
+class Remove(Handles):
+    def func(self, action):
+        pattern = input("Remove (contact name or phone): ")
+        return self.book.remove(pattern)
+
+
+class Save(Handles):
+    def func(self, action):
+        file_name = input("File name: ")
+        return self.book.save(file_name)
+
+
+class Load(Handles):
+    def func(self, action):
+        file_name = input("File name: ")
+        return self.book.load(file_name)
+
+
+class Congratulate(Handles):
+    def func(self, action):
+        print(self.book.congratulate())
+
+
+class View(Handles):
+    def func(self, action):
+        print(self.book)
+
+
+class Exit(Handles):
+    def func(self, action):
+        pass
+
+
+class Bot:
+    def __init__(self):
+        self.book = AddressBook()
+
+    def handle(self, action):
+        if action == 'add':
+            add = Add()
+            add.func(action)
+        elif action == 'search':
+            search = Search()
+            search.func(action)
+        elif action == 'edit':
+            edit = Edit()
+            edit.func(action)
+        elif action == 'remove':
+            remove = Remove()
+            remove.func(action)
+        elif action == 'save':
+            save = Save()
+            save.func(action)
+        elif action == 'load':
+            load = Load()
+            load.func(action)
+        elif action == 'congratulate':
+            congratulate = Congratulate()
+            congratulate.func(action)
+        elif action == 'view':
+            view = View()
+            view.func(action)
+        elif action == 'exit':
+            exit = Exit()
+            exit.func(action)
+        else:
+            print("There is no such command!")
+
